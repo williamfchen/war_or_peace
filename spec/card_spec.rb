0d9fea79c1641d03543a3d2cf3f0c3f316517dfa
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-11-30 14:08:13 -0700

    add spec starter

---
+++ spec/card_spec.rb
@@ -0,0 +1,17 @@
+require './lib/card'
+
+RSpec.describe Card do
+  it "exists" do
+    card = Card.new(:diamond, 'Queen', 12)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Queen', 12)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Queen')
+    expect(card.rank).to eq(12)
+  end
+end
