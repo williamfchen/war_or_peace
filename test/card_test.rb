c9a1f7deb7e7ff654842329a338b18c1d65eb028
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2021-02-01 13:28:31 -0700

    Update card_test.rb

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,10 +3,6 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardTest < Minitest::Test
-  def setup
-    card = Card.new(:diamond, 'Queen', 12)
-  end
-
   def test_it_exists
     card = Card.new(:diamond, 'Queen', 12)
 
